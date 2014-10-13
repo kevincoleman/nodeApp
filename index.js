dc6169bc5b7124820e5b4d60bd2d0374410df938
Kevin Coleman <kevin.n.coleman@gmail.com>
2014-10-12 17:59:47 -0700

    server working, added log line to note server start

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ var Hapi = require('hapi');
 // Create a server with a host and port
 var server = new Hapi.Server('localhost', 8000);
 
+// Log server running
+console.log('Server running at:', server.info.uri);
+
 // Add the route
 server.route({
     method: 'GET',
     path: '/hello',
     handler: function (request, reply) {
 
-        reply('hello world');
+        reply('laterList will be a thing.');
     }
 });
 
