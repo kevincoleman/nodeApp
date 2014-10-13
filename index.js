0570a10dec5dde51079c0f93670ef1666d2dd02c
Kevin Coleman <kevin.n.coleman@gmail.com>
2014-10-12 21:48:24 -0700

    grouped routes into array

--- index.js
+++ index.js
@@ -4,23 +4,24 @@ var Good = require('good');
 // Create a server with a host and port
 var server = new Hapi.Server('localhost', 8000);
 
-// / to main
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: function (request, reply) {
-    reply('laterList will be a thing.');
-  }
-});
+var routes = [
+    {
+      method: 'GET',
+      path: '/',
+      handler: function (request, reply) {
+        reply('laterList will be a thing.');
+      }
+    },
+    {
+      method: 'GET',
+      path: '/{name}',
+      handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+      }
+    }
+  ];
 
-// /name to name script
-server.route({
-  method: 'GET',
-  path: '/{name}',
-  handler: function (request, reply) {
-    reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
-  }
-});
+server.route(routes);
 
 server.pack.register(Good, function (err) {
   if (err) {
